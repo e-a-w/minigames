8f3e9d79385fc2ad6250127bc45af9415015f655
e-a-w- <eawatkins.0@gmail.com>
2022-07-12 18:41:48 -0700

    initialize with games scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+Game.create([
+  {
+    name: 'Matching',
+    description: 'a fun matching game',
+    rules: 'Match the cards'
+  },
+  {
+    name: 'Go Fish',
+    description: 'a fun fishing game',
+    rules: 'Find the fish'
+  },
+  {
+    name: 'Waar',
+    description: 'a fun battling game',
+    rules: 'Take all the cards'
+  },
+])
